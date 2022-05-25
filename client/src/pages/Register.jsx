38b00e989171d688314cb358f809ec7f1c9fa362
hitesh-23 <hiteshchhatvani@gmail.com>
2022-05-25 18:50:23 +0530

    feat: changed test cases

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,80 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 export const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+  //global state and useNavigate
+  const { showAlert, displayAlert } = useAppContext();
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { email, password, isMember, name } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        {showAlert && (
+          <Alert alertType="alert-danger" alertMessage="Enter all the fields" />
+        )}
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {/* Name input */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+            labelText="Name"
+          />
+        )}
+        {/* Email input */}
+
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+          labelText="Email"
+        />
+        {/* Password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+          labelText="Password"
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member"}
+          <button type="button" className="member-btn" onClick={toggleMember}>
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
